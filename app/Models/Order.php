a5f16a476eea163879037d7882dee2d27c8eb4c9
nikolastijepic <nikola.stijepic94@icloud.com>
2026-08-10 22:26:58 +0200

    feat: add orders

---
+++ app/Models/Order.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+#[Fillable(['user_id', 'subtotal', 'shipping', 'total', 'status',])]
+
+class Order extends Model
+{
+    protected function casts(): array
+    {
+        return [
+            'subtotal' => 'decimal:2',
+            'shipping' => 'decimal:2',
+            'total' => 'decimal:2',
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
